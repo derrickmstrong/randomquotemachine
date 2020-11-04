e438e20d00cd53c584677294cc1609c48019d4d6
Derrick Strong <derrickmstrong@gmail.com>
2020-11-04 09:51:20 -0600

    Updates

---
+++ precache-manifest.e1999cfe1ba2547aa4c38c0c973999d1.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de010518d7fee27c6525cee1c42620c",
+    "url": "/randomquotemachine/index.html"
+  },
+  {
+    "revision": "4764e0986e311b004399",
+    "url": "/randomquotemachine/static/css/2.a3863f9a.chunk.css"
+  },
+  {
+    "revision": "1d5c7459a4f8f8d73e2b",
+    "url": "/randomquotemachine/static/css/main.4b58814a.chunk.css"
+  },
+  {
+    "revision": "4764e0986e311b004399",
+    "url": "/randomquotemachine/static/js/2.5a70b81d.chunk.js"
+  },
+  {
+    "revision": "55529b20611b073b55672a2114e7f5e3",
+    "url": "/randomquotemachine/static/js/2.5a70b81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d5c7459a4f8f8d73e2b",
+    "url": "/randomquotemachine/static/js/main.f9710e3e.chunk.js"
+  },
+  {
+    "revision": "cd043ab3bb0684cdfa57",
+    "url": "/randomquotemachine/static/js/runtime-main.0380554f.js"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/randomquotemachine/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/randomquotemachine/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/randomquotemachine/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/randomquotemachine/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/randomquotemachine/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/randomquotemachine/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/randomquotemachine/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/randomquotemachine/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/randomquotemachine/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/randomquotemachine/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/randomquotemachine/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/randomquotemachine/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/randomquotemachine/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/randomquotemachine/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/randomquotemachine/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/randomquotemachine/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/randomquotemachine/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/randomquotemachine/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/randomquotemachine/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/randomquotemachine/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/randomquotemachine/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/randomquotemachine/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/randomquotemachine/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/randomquotemachine/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  }
+]);

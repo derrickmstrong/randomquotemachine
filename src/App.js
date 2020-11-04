ad260ce8c49d407ceac9a79651ec766b3074603f
Derrick Strong <derrickmstrong@gmail.com>
2020-11-04 09:42:24 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { random } from 'lodash';
+import 'typeface-roboto';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core';
+
+import QuoteMachine from './components/QuoteMachine';
+
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'center',
+  },
+};
+
+function App({ classes }) {
+  const [quotes, setQuotes] = useState([]);
+  const [selectedQuoteIndex, setSelectedQuoteIndex] = useState(null);
+  const [colorHolder, setColorHolder] = useState('black');
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        'https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json'
+      );
+      const quotes = await data.json();
+      setQuotes(quotes);
+      setSelectedQuoteIndex(random(0, quotes.length - 1));
+    }
+    fetchData();
+  }, []);
+
+  function getSelectedQuote() {
+    if (!quotes.length || !Number.isInteger(selectedQuoteIndex)) {
+      return undefined;
+    }
+    return quotes[selectedQuoteIndex];
+  }
+  /**
+   * Returns an integer between 0 and 102 representing an index in state.quotes
+   **/
+  function generateNewQuoteIndex() {
+    if (!quotes.length) {
+      return undefined;
+    }
+    return random(0, quotes.length - 1);
+  }
+
+  /**
+   * Sets the state for selectedQuoteIndex
+   **/
+  function assignNewQuoteIndex() {
+    const colorCode =
+      'rgb(' +
+      random(0, 256) +
+      ',' +
+      random(0, 256) +
+      ',' +
+      random(0, 256) +
+      ')';
+    setColorHolder(colorCode);
+
+    setSelectedQuoteIndex(generateNewQuoteIndex());
+  }
+
+  return (
+    <Grid
+      className={classes.container}
+      style={{
+        backgroundColor: colorHolder,
+      }}
+      id='quote-box'
+      justify='center'
+      container>
+      <Grid xs={8} large={6} item>
+        {getSelectedQuote() ? (
+          <QuoteMachine
+            selectedQuote={getSelectedQuote()}
+            assignNewQuoteIndex={assignNewQuoteIndex}
+            colorHolder={colorHolder}
+          />
+        ) : null}
+      </Grid>
+    </Grid>
+  );
+}
+
+export default withStyles(styles)(App);
---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const QuoteMachine = props => (
+  <Card>
+    <CardContent>
+      <Typography>
+        <span id="text">{props.selectedQuote.quote}</span>
+        <span id="author"> - {props.selectedQuote.author}</span>
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button
+        style={{
+          backgroundColor: props.colorHolder,
+          color: '#fff',
+          textShadow: '.5px .5px #707070'
+        }}
+        size="small"
+        onClick={props.assignNewQuoteIndex}
+        id="new-quote"
+      >
+        Next Quote
+      </Button>
+      <IconButton
+        href={encodeURI(
+          `https://twitter.com/intent/tweet?text=${props.selectedQuote.quote} - ${props.selectedQuote.author}&hashtags=CodingIsMyLife`
+        )}
+        target="_blank"
+        id="tweet-quote"
+      >
+        <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+      </IconButton>
+    </CardActions>
+  </Card>
+);
+
+export default QuoteMachine;
